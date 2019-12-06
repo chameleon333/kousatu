589939a1e0a72c4811808183a1d65cbb3587d921
chameleon333 <uta3chame@gmail.com>
2019-12-06 19:43:23 +0900

    [update]index.blade.phpデザイン変更

--- resources/views/articles/index.blade.php
+++ resources/views/articles/index.blade.php
@@ -11,56 +11,18 @@
     <div class="col-md-8 mb-3">
       <div class="card">
         <div class="card-haeder p-3 w-100 d-flex">
-          <img src="{{ asset('storage/profile_image/' .$timeline->user->profile_image) }}" class="rounded-circle" width="50" height="50">
+        <a href="{{ url('users/' .$timeline->user->id) }}" class="text-secondary">
+          <img src="{{ asset('storage/profile_image/' .$timeline->user->profile_image) }}" class="rounded" width="50" height="50">
+        </a>
           <div class="ml-2 d-flex flex-column">
-            <p class="mb-0">{{ $timeline->user->name }}</p>
-            <a href="{{ url('users/' .$timeline->user->id) }}" class="text-secondary">{{ $timeline->user->screen_name }}</a>
-          </div>
-          <div class="d-flex justify-content-end flex-grow-1">
-            <p class="mb-0 text-secondary">{{ $timeline->created_at->format('Y-m-d H:i') }}</p>
-          </div>
-        </div>
-        <div class="card-body">
-          {!! nl2br(e($timeline->body)) !!}
-        </div>
-        <div class="card-footer py-1 d-flex justify-content-end bg-white">
-          @if ($timeline->user->id === Auth::user()->id)
-          <div class="dropdown mr-3 d-flex align-items-center">
-            <a href="{{ url('articles/' .$timeline->id. '/edit') }}" role="button" id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-              <i class="fas fa-ellipsis-v fa-fw"></i>
-            </a>
-            <div class="dropdown-menu" aria-labelledby="dropdownMenuLink">
-              <form method="POST" action="{{ url('articles/' .$timeline->id) }}" class="mb-0">
-                @csrf
-                @method('DELETE')
-
-                <a href="{{ url('articles/' .$timeline->id .'/edit') }}" class="dropdown-item">編集</a>
-                <button type="submit" class="dropdown-item del-btn">削除</button>
-              </form>
-            </div>
-          </div>
-          @endif
-          <div class="mr-3 d-flex align-items-center">
-            <a href="{{ url('articles/' .$timeline->id) }}"><i class="far fa-comment fa-fw"></i></a>
-            <p class="mb-0 text-secondary">{{ count($timeline->comments) }}</p>
-          </div>
-
-          <div class="d-flex align-items-center">
-            @if (!in_array($user->id, array_column($timeline->favorites->toArray(), 'user_id'), TRUE))
-              <form method="POST" action="{{ url('favorites/') }}" class ="mb-0">
-                @csrf
-
-                <input type="hidden" name="article_id" value="{{ $timeline->id }}">
-                <button type="submit" class="btn p-0 border-0 text-primary"><i class="far fa-heart fa-fw"></i></button>
-              </form>
-            @else
-              <form method="POST" action="{{ url('favorites/' .array_column($timeline->favorites->toArray(), 'id', 'user_id')[$user->id]) }}" class="mb-0">
-              @csrf
-              @method('DELETE')
-                <button type="submit" class="btn p-0 border-0 text-danger"><i class="fas fa-heart fa-fw"></i></button>
-              </form>
-            @endif
-            <p class="mb-0 text-secondary">{{ count($timeline->favorites) }}</p>
+          <a href="{{ route('articles.show', ['article'=>$timeline->id]) }}">
+            <p class="mb-0">{{ $timeline->title }}</p>
+          </a>
+            <p class="mb-0 text-secondary">
+              <span>By {{$timeline->user->name}}</span>
+              <span>{{ $timeline->created_at->format('Y-m-d H:i') }}</span>
+              <span><i class="far fa-thumbs-up"></i>{{ count($timeline->favorites) }}</span>
+            </p>
           </div>
         </div>
       </div>
