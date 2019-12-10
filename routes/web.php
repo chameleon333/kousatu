314f625f2f1fae7b540ec1dfd6217ff67a79c8e7
chameleon333 <uta3chame@gmail.com>
2019-12-10 18:59:57 +0900

    [update]articles ルーティング切り分け

--- routes/web.php
+++ routes/web.php
@@ -12,7 +12,8 @@
 */
 
 Route::get('/', function () {
-    return view('welcome');  
+    // return view('articles/index');  
+    return redirect('articles');
 });
 
 //Route::resource('articles', 'ArticlesController');
@@ -21,10 +22,14 @@ Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
 
+Route::resource('articles', 'ArticlesController',['only' => ['index','show']]);
+// Route::get('articles', 'ArticlesController@index');
+// Route::get('articles/{article}/', 'ArticlesController@show');
+
 
 #ログイン状態
 Route::group(['middleware' => 'auth'], function() {
-//  #ユーザ関連
+  //#ユーザ関連
   Route::resource('users', 'UsersController',['only' => ['index', 'show', 'edit', 'update']]);
   
   // フォロー/フォロー解除を追加
@@ -32,7 +37,8 @@ Route::group(['middleware' => 'auth'], function() {
   Route::delete('users/{user}/unfollow', 'UsersController@unfollow')->name('unfollow');
   
   //投稿記事関連
-  Route::resource('articles', 'ArticlesController',['only' => ['index', 'create', 'store', 'show', 'edit', 'update', 'destroy']]);
+  // Route::resource('articles', 'ArticlesController',['only' => ['index', 'create', 'store', 'show', 'edit', 'update', 'destroy']]);
+  Route::resource('articles', 'ArticlesController',['only' => ['create', 'store', 'edit', 'update', 'destroy']]);
 
   //コメント関連
   Route::resource('comments', 'CommentsController', ['only' => ['store']]);
