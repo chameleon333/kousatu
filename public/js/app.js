6e08767f253ac2d1f4c7a2bcce3ee4470abd9e02
chameleon333 <uta3chame@gmail.com>
2020-01-02 15:19:39 +0900

    [add]npm-check-updates

--- public/js/app.js
+++ public/js/app.js
@@ -49613,8 +49613,8 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! /Users/chameleon/code/go_out/resources/js/app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! /Users/chameleon/code/go_out/resources/sass/app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! /home/vagrant/code/go_out/resources/js/app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! /home/vagrant/code/go_out/resources/sass/app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
