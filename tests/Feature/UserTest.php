d2004fee6a973276f5ed4e45f1f5712c7fea00bf
chameleon333 <uta3chame@gmail.com>
2020-01-18 17:57:02 +0900

    [add]テストプログラム: いいね機能

--- tests/Feature/UserTest.php
+++ tests/Feature/UserTest.php
@@ -1,10 +1,11 @@
 <?php
 
-namespace Tests\Feature;
+// namespace Tests\Feature;
 
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
+
 
 class UserTest extends TestCase
 {
@@ -18,4 +19,34 @@ class UserTest extends TestCase
         $response = $this->get('/');
         $response->assertStatus(302);
     }
+
+    public function testPostLike()
+    {
+        #いいねできているかチェック
+        $factory_user = factory(App\Models\User::class)->create();
+        $article = factory(App\Models\Article::class)->create();
+        $user_id = $factory_user->id;
+        $article_id = $article->id;
+        $response = $this->actingAs($factory_user);
+        // $response->get('/articles/'.$id);
+        $response->post('/favorites', ['article_id' => $article_id]);
+        $response->assertDatabaseHas('favorites', [
+            'user_id' => $user_id,
+            'article_id' => $article_id
+        ]);
+
+        #いいねを外せているかチェック
+        // SELECT
+        $favorite = \App\Models\Favorite::where('user_id',$user_id)->where('article_id',$article_id)->first();
+        $this->assertNotNull($favorite); // データが取得できたかテスト
+        $favorite_id =$favorite->id;
+
+        $response->delete('/favorites/'.$favorite_id);
+        $response->assertDatabaseMissing('favorites', [
+            'user_id' => $user_id,
+            'article_id' => $article_id
+        ]);
+    }
+
+
 }
