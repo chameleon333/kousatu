76be7cec5c4ecfd49a7a4739de78f74668ae2bcd
chameleon333 <uta3chame@gmail.com>
2020-04-16 21:08:00 +0900

    [add]ArticleFactory use App\Models\User 追加

--- database/factories/ArticleFactory.php
+++ database/factories/ArticleFactory.php
@@ -2,13 +2,14 @@
 
 /** @var \Illuminate\Database\Eloquent\Factory $factory */
 
+use App\Models\User;
 use App\Models\Article;
 use Faker\Generator as Faker;
 
 $factory->define(Article::class, function (Faker $faker) {
     return [
         'user_id' => function(){
-            return factory(App\Models\User::class)->create()->id;
+            return factory(User::class)->create()->id;
         },
         'title' => $faker->text,
         'header_image' => "test",
