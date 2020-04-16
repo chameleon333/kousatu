3d2153d52c81c85126a8df77ff0801394745d9ab
chameleon333 <uta3chame@gmail.com>
2020-04-17 08:59:59 +0900

    [add]記事更新テスト追加

--- tests/Feature/UserTest.php
+++ tests/Feature/UserTest.php
@@ -152,7 +152,7 @@ class UserTest extends TestCase
         ]);
     }
 
-    public function testPostArticle()
+    public function testCreateArticle()
     {
         #アップロードした記事データがDBにあるかテスト
         $factory_user = factory(App\Models\User::class)->create();
@@ -202,6 +202,75 @@ class UserTest extends TestCase
         }
 
     } 
+
+    public function testUpdateArticle()
+    {
+        #更新した記事データがDBにあるかテスト
+        $factory_user = factory(App\Models\User::class)->create();
+
+        $factory_article = factory(Article::class)
+        ->create(["user_id" => $factory_user->id,])
+        ->each(function(Article $article){
+            $article->tags()->saveMany(factory(Tag::class, rand(0,5)))->create();
+
+            $tags = Tag::all();
+            $tag_ids = [];
+            foreach($tags as $tag) {
+                $tag_ids[] = $tag->id;
+            }
+            $article->articleTagSync($tag_ids);
+        });
+
+        $response = $this->actingAs($factory_user);
+
+        $user_id = $factory_user->id;
+        $testTitle = "テストタイトル";
+        $testBody = "# テストh1
+## テストタイトルh2
+- テストリスト
+- テストリスト
+- テストリスト
+- テストリスト";
+
+        $testTags = ["テスト1","テスト2","テスト3","テスト4","テスト5"];
+        $response->put("/articles/{$factory_article}", [ 'title' => $testTitle, 'body' => $testBody,'tags' => $testTags]);
+        $response->assertDatabaseHas('articles', [
+            'title' => $testTitle,
+            'body' => $testBody,
+        ]);
+        
+        #登録したタグ名が登録テーブルに登録されているかテスト
+        foreach($testTags as $testTag){
+            $response->assertDatabaseHas('tags', [
+                'name' => $testTag,
+            ]);
+            $tag_id = Tag::select('id')->where("name",$testTag)->first();
+            $tag_ids[] = $tag_id->id;
+        }
+
+        #登録したカテゴリーが中間テーブルに保存されているかテスト
+        foreach($tag_ids as $tag_id){
+            $response->assertDatabaseHas('article_tag',[
+                'article_id' => $factory_article,
+                'tag_id' => $tag_id
+            ]);
+        }
+
+        #不正な形式の記事が登録されないかテスト
+        #タイトル0文字または３1文字以上の記事が投稿されないかテスト
+        $lengths = [0,31];
+        foreach($lengths as $length) {
+            $Bad_testTitle = substr(str_shuffle(str_repeat('0123456789abcdefghijklmnopqrstuvwxyz', $length)), 0, $length);
+
+            $response->post('/articles', ['title' => $Bad_testTitle, 'body' => $testBody]);
+            $response->assertDatabaseMissing('articles', [
+                'title' => $Bad_testTitle,
+                'body' => $testBody
+            ]);
+        }
+
+    } 
+
 
     
 }
