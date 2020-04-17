8bfbc9a7aa7b2f41937f9ea367c62666825725e3
chameleon333 <uta3chame@gmail.com>
2020-04-17 09:28:16 +0900

    [update]change email screen_name in Seeder and app.blade

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -17,11 +17,11 @@ class UsersTableSeeder extends Seeder
     {
       #簡単ログイン用 ユーザー作成
       User::create([
-        'screen_name' => 'test1',
+        'screen_name' => 'taro',
         'name'           => '山田太郎',
         'self_introduction'  => "私は山田太郎です。映画、小説、漫画。さまざまなコンテンツの考察を投稿します。",
         'profile_image'  => 'storage/profile_image/profile1.jpeg',
-        'email'          => 'test1@test.com',
+        'email'          => 'taro@gmail.com',
         'password'       => Hash::make('12345678'),
         'remember_token' => Str::random(10),
         'created_at'     => now(),
@@ -29,7 +29,7 @@ class UsersTableSeeder extends Seeder
       ]);
 
       $faker = Faker::create('ja_JP');
-      for ($i = 1; $i <= 10; $i++){
+      for ($i = 2; $i <= 10; $i++){
         $name = $faker->name;
         User::create([
           'screen_name' => $faker->unique()->regexify('\w{8}'),
