7a9b475251603b5403804ebf62f64aa6c3a926d9
chameleon333 <uta3chame@gmail.com>
2020-05-19 14:14:25 +0900

    [update]alb by terraform

--- terraform/alb/main.tf
+++ terraform/alb/main.tf
@@ -1,13 +1,8 @@
-provider "aws" {
-  region  = "ap-northeast-1"
-  version = "2.8"
-}
-
 terraform {
   required_version = "0.12.24"
   backend "s3" {
     bucket = "kousatu-private"
-    key    = "terraform/terraform.tfstate"
+    key    = "terraform/alb/terraform.tfstate"
     region = "ap-northeast-1"
   }
 }
---
+++ terraform/alb/provider.tf
@@ -0,0 +1,4 @@
+provider "aws" {
+    region = "ap-northeast-1"
+    version = "2.8"
+}
