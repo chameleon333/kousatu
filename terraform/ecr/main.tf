5ca4f3a050e7a90c4bdced0dea688f5758055e26
chameleon333 <uta3chame@gmail.com>
2020-05-19 13:20:49 +0900

    [add]ecr by terraform

---
+++ terraform/ecr/main.tf
@@ -0,0 +1,11 @@
+provider "aws" {
+  region = "ap-northeast-1"
+}
+
+terraform {
+  backend "s3" {
+    bucket = "kousatu-private"
+    key    = "terraform/ecr/terraform.tfstate"
+    region = "ap-northeast-1"
+  }
+}
