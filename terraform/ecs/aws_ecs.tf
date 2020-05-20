9dbb056a7844b76f97d83c5a43bdcd90a169ab90
chameleon333 <uta3chame@gmail.com>
2020-05-20 19:40:31 +0900

    [update]ecs by terraform

--- terraform/ecs/aws_ecs.tf
+++ terraform/ecs/aws_ecs.tf
@@ -27,6 +27,79 @@ resource "aws_ecs_service" "kousatu" {
 
 }
 
+
+resource "aws_launch_configuration" "kousatu" {
+  image_id = "ami-00c408a8b71d5c614"
+  instance_type               = "t2.micro"
+  user_data = file("./user_data.sh")
+  iam_instance_profile        = data.terraform_remote_state.iam.outputs.aws_iam_instance_profile_arn
+  security_groups             = [data.terraform_remote_state.vpc.outputs.security_group_id]
+  associate_public_ip_address = true
+
+}
+
+resource "aws_autoscaling_group" "kousatu" {
+  max_size                  = 2
+  min_size                  = 0
+  health_check_grace_period = 0
+  health_check_type         = "EC2"
+  launch_configuration = aws_launch_configuration.kousatu.id
+  # tag {
+  #     key                 = "Description"
+  #     propagate_at_launch = true
+  #     value               = "This instance is the part of the Auto Scaling group which was created through ECS Console"
+  #   }
+  # tag {
+  #     key                 = "Name"
+  #     propagate_at_launch = true
+  #     value               = "ECS Instance - EC2ContainerService-kousatu-cluster"
+  #   }
+  # tag {
+  #     key                 = "aws:cloudformation:logical-id"
+  #     propagate_at_launch = true
+  #     value               = "EcsInstanceAsg"
+  #   }
+  # tag {
+  #     key                 = "aws:cloudformation:stack-id"
+  #     propagate_at_launch = true
+  #     value               = "arn:aws:cloudformation:ap-northeast-1:636153449311:stack/EC2ContainerService-kousatu-cluster/c1263090-5d4a-11ea-8894-0693e5f01c30"
+  #   }
+  # tag {
+  #     key                 = "aws:cloudformation:stack-name"
+  #     propagate_at_launch = true
+  #     value               = "EC2ContainerService-kousatu-cluster"
+  #   }
+}
+
+resource "aws_appautoscaling_target" "kousatu" {
+  min_capacity       = 1
+  max_capacity       = 2
+  resource_id        = "service/kousatu-cluster/ecs-kousatu-service"
+  scalable_dimension = "ecs:service:DesiredCount"
+  service_namespace  = "ecs"
+}
+
+
+resource "aws_appautoscaling_policy" "kousatu" {
+  name = "tracking"
+  service_namespace = aws_appautoscaling_target.kousatu.service_namespace
+  scalable_dimension = aws_appautoscaling_target.kousatu.scalable_dimension
+  resource_id = aws_appautoscaling_target.kousatu.resource_id
+  policy_type = "TargetTrackingScaling"
+
+  target_tracking_scaling_policy_configuration {
+    disable_scale_in   = false
+    scale_in_cooldown  = 300
+    scale_out_cooldown = 300
+    target_value       = 75
+
+    predefined_metric_specification {
+        predefined_metric_type = "ECSServiceAverageCPUUtilization"
+      }
+  }
+
+}
+
 data "template_file" "service_container_definition" {
   template = file("./task-definitions/service.json")
 }
@@ -42,12 +115,19 @@ resource "aws_ecs_task_definition" "kousatu" {
   requires_compatibilities = [
     "EC2",
   ]
-
   volume {
     name = "src"
   }
 }
 
+data "terraform_remote_state" "vpc" {
+  backend = "s3"
+  config = {
+    bucket = "kousatu-private"
+    key    = "terraform/vpc/terraform.tfstate"
+    region = "ap-northeast-1"
+  }
+}
 
 data "terraform_remote_state" "iam" {
   backend = "s3"
