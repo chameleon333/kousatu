5fa475d089075211531409d2846d07e353618a89
chameleon333 <uta3chame@gmail.com>
2020-05-23 16:09:35 +0900

    [add]db_connection for circleci

--- config/database.php
+++ config/database.php
@@ -91,6 +91,23 @@ return [
             'prefix_indexes' => true,
         ],
 
+        // 末尾に追加
+        // CircleCI テスト用
+        'circle_testing' => [
+            'driver' => 'mysql',
+            'host' => '127.0.0.1',
+            'port' => '3306',
+            'database' => 'circle_test',
+            'username' => 'root',
+            'password' => '',
+            'charset' => 'utf8mb4',
+            'collation' => 'utf8mb4_unicode_ci',
+            'prefix' => '',
+            'prefix_indexes' => true,
+            'strict' => true,
+            'engine' => null,
+        ],
+
     ],
 
     /*
