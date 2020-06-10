a6116b02075c6965a69584df88aa465e3c43f9d7
chameleon333 <uta3chame@gmail.com>
2020-06-11 07:54:18 +0900

    [fix]show.blade.php users

--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -1,13 +1,7 @@
 @extends('layouts.app')
 
 @section('content')
-<?php 
-  if(Auth::user()) {
-    $auth = Auth::user()->id; 
-  } else {
-    $auth = null;
-  }
-?>
+
 
 <div class="container">
   <div class="row justify-content-center">
@@ -24,21 +18,21 @@
           <div class="m-3 d-flex flex-column justify-content-between">
             <div class="d-flex">
               <div>
-                @if ($user->id === $auth)
+                @if (auth()->user())
                 <a href="{{ url('users/' .$user->id .'/edit') }}" class="btn btn-primary">プロフィールを編集する</a>
                 @else
-                @if ($is_following)
-                <form action="{{ route('users.unfollow', ['user' => $user->id]) }}" method="POST">
-                  {{ csrf_field() }}
-                  {{ method_field('DELETE') }}
+                  @if ($is_following)
+                  <form action="{{ route('users.unfollow', ['user' => $user->id]) }}" method="POST">
+                    {{ csrf_field() }}
+                    {{ method_field('DELETE') }}
 
-                  <button type="submit" class="btn btn-danger">フォロー解除</button>
-                </form>
+                    <button type="submit" class="btn btn-danger">フォロー解除</button>
+                  </form>
                 @else
-                <form action="{{ route('users.follow', ['user' => $user->id]) }}" method="POST">
-                  {{ csrf_field() }}
-                  <button type="submit" class="btn btn-sm page-link text-dark d-inline-block">フォローする</button>
-                </form>
+                  <form action="{{ route('users.follow', ['user' => $user->id]) }}" method="POST">
+                    {{ csrf_field() }}
+                    <button type="submit" class="btn btn-sm page-link text-dark d-inline-block">フォローする</button>
+                  </form>
                 @endif
                   @if ($is_followed)
                     <span class="mt-2 px-1 bg-white text-dark">フォローされています</span>
@@ -47,7 +41,7 @@
               </div>
             </div>
             <div class="d-flex">
-            <span class="mt-2">{{ $user->self_introduction }}</span>
+              <span class="mt-2">{{ $user->self_introduction }}</span>
             </div>
             <div class="d-flex">
               <div class="d-flex flex-column">                
@@ -62,78 +56,75 @@
       </div>
     </div>
     
-    @if ($user->id === $auth)
-    <div class="col-md-10 mb-3">
-      <div class="col-md-4">
-        <select class="form-control" name="select" onChange="location.href=value;">
-          @foreach ($status_list as $status_id => $status_text)
-          <option @if($request_status_id == $status_id) selected @endif value="{{ url()->current() }}?status={{ $status_id }}">{{ $status_text }}</option>
-          @endforeach
-        </select>
+    @if (auth()->user())
+      <div class="col-md-10 mb-3">
+        <div class="col-md-4">
+          <select class="form-control" name="select" onChange="location.href=value;">
+            @foreach ($status_list as $status_id => $status_text)
+            <option @if($request_status_id == $status_id) selected @endif value="{{ url()->current() }}?status={{ $status_id }}">{{ $status_text }}</option>
+            @endforeach
+          </select>
+        </div>
       </div>
-    </div>
     @endif
     
     <div class="col-md-10 mb-3 row">
     @if (isset($timelines))
-    @foreach ($timelines as $timeline)
-      <div class="p-3 col-sm-6">
-        <div class="card">
-          <a href="{{ route('articles.show', ['article'=>$timeline->id]) }}">
-            <div class="header-image-wrapper">
-              <div class="header-image-content" style="background-image:url('{{$timeline->header_image}}');"></div>    
-            </div>
-          </a>
-          <div class="card-haeder w-100 p-3">
-            <div class="ml-2 d-flex flex-column">
-              <div class="w-100 d-inline-flex">
-                <a href="{{ url('users/' .$timeline->user->id) }}" class="text-secondary">
-                  <img src="{{ asset($timeline->user->profile_image) }}" class="rounded" width="39" height="39">
-                </a>
-                <a href="{{ route('articles.show', ['article'=>$timeline->id]) }}">
-                  <p class="px-1">{{ $timeline->title }}</p>
-                </a>
+      @foreach ($timelines as $timeline)
+        <div class="p-3 col-sm-6">
+          <div class="card">
+            <a href="{{ route('articles.show', ['article'=>$timeline->id]) }}">
+              <div class="header-image-wrapper">
+                <div class="header-image-content" style="background-image:url('{{$timeline->header_image}}');"></div>    
               </div>
-              <p class="mb-0 text-secondary">
-                @foreach($timeline->tags as $tag)
-                  <span class="tag-mark">{{$tag->name}}</span>
-                @endforeach
-              </p>
-              <div class="mt-1 d-flex">
-                <div class="mr-auto text-secondary">
-                  <span>by &#064;{{$timeline->user->screen_name}}</span>
-                  <span>{{ $timeline->created_at->format('Y-m-d H:i') }}</span>
-                  <span><i class="far fa-thumbs-up"></i>{{ count($timeline->favorites) }}</span>
+            </a>
+            <div class="card-haeder w-100 p-3">
+              <div class="ml-2 d-flex flex-column">
+                <div class="w-100 d-inline-flex">
+                  <a href="{{ url('users/' .$timeline->user->id) }}" class="text-secondary">
+                    <img src="{{ asset($timeline->user->profile_image) }}" class="rounded" width="39" height="39">
+                  </a>
+                  <a href="{{ route('articles.show', ['article'=>$timeline->id]) }}">
+                    <p class="px-1">{{ $timeline->title }}</p>
+                  </a>
                 </div>
-                @if ($user->id === $auth)
-                  <div class="dropdown d-flex align-items-center">
-                    <a href="{{ url('articles/' .$timeline->id. '/edit') }}" role="button" id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                      <i class="fas fa-ellipsis-v fa-fw"></i>
-                    </a>
-                    <div class="dropdown-menu" aria-labelledby="dropdownMenuLink">
-                      <form method="POST" action="{{ url('articles/' .$timeline->id) }}" class="mb-0">
-                        @csrf
-                        @method('DELETE')
-
-                        <a href="{{ url('articles/' .$timeline->id .'/edit') }}" class="dropdown-item">編集</a>
-                        <button type="submit" class="dropdown-item del-btn">削除</button>
-                      </form>
-                    </div>
+                <p class="mb-0 text-secondary">
+                  @foreach($timeline->tags as $tag)
+                    <span class="tag-mark">{{$tag->name}}</span>
+                  @endforeach
+                </p>
+                <div class="mt-1 d-flex">
+                  <div class="mr-auto text-secondary">
+                    <span>by &#064;{{$timeline->user->screen_name}}</span>
+                    <span>{{ $timeline->created_at->format('Y-m-d H:i') }}</span>
+                    <span><i class="far fa-thumbs-up"></i>{{ count($timeline->favorites) }}</span>
                   </div>
-                @endif
-                <div class="d-flex align-items-center">
-                  <a href="{{ url('articles/' .$timeline->id) }}#comment"><i class="far fa-comment fa-fw"></i></a>
-                  <p class="mb-0 text-secondary">{{ count($timeline->comments) }}</p>
+                  @if (auth()->user())
+                    <div class="dropdown d-flex align-items-center">
+                      <a href="{{ url('articles/' .$timeline->id. '/edit') }}" role="button" id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        <i class="fas fa-ellipsis-v fa-fw"></i>
+                      </a>
+                      <div class="dropdown-menu" aria-labelledby="dropdownMenuLink">
+                        <form method="POST" action="{{ url('articles/' .$timeline->id) }}" class="mb-0">
+                          @csrf
+                          @method('DELETE')
+
+                          <a href="{{ url('articles/' .$timeline->id .'/edit') }}" class="dropdown-item">編集</a>
+                          <button type="submit" class="dropdown-item del-btn">削除</button>
+                        </form>
+                      </div>
+                    </div>
+                  @endif
+                  <div class="d-flex align-items-center">
+                    <a href="{{ url('articles/' .$timeline->id) }}#comment"><i class="far fa-comment fa-fw"></i></a>
+                    <p class="mb-0 text-secondary">{{ count($timeline->comments) }}</p>
+                  </div>
                 </div>
               </div>
             </div>
           </div>
         </div>
-      </div>
-
-
-
-    @endforeach
+      @endforeach
     @endif
     </div>
   </div>
