c7ff2e88605d62ee91b76c21e0bac1060b50fb46
chameleon333 <uta3chame@gmail.com>
2020-06-16 16:30:10 +0900

    [add]popular user to tags.show

--- resources/views/tags/show.blade.php
+++ resources/views/tags/show.blade.php
@@ -18,8 +18,8 @@
       </div>
       <div class="col-md-3 order-md-1">
         @include('components.popular_tag_list')
+        @include('components.popular_user_list')
       </div>
-
     </div>
   </div>
   
