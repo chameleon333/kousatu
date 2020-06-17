3e949e7a4bdbb46a21e1f7976018735794f13375
chameleon333 <uta3chame@gmail.com>
2020-06-18 05:10:16 +0900

    [update]title size to search.index

--- resources/views/search/index.blade.php
+++ resources/views/search/index.blade.php
@@ -10,7 +10,7 @@
     </div>
     <div class="row col-md-12">
       <div class="col-md-9 mb-3 order-md-2">
-        <h2 class="text-center"><span class="double-quotation-mark">{{$keyword}}</span><span class="ml-2">で検索</span></h2>
+        <h2 class="text-center"><span class="double-quotation-mark h2">{{$keyword}}</span><span class="ml-2">で検索</span></h2>
         <hr>
         @include('components.article_list')
       </div>
