4e9be5e4b101ab62f88e0f9c42611ce27edce15e
chameleon333 <uta3chame@gmail.com>
2020-06-18 07:09:24 +0900

    [update]html in show.blade

--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -1,11 +1,9 @@
 @extends('layouts.app')
 
 @section('content')
-
-
 <div class="container">
   <div class="row justify-content-center">
-    <div class="col-md-10 mb-3">
+    <div class="col-md-12 mb-3">
       <div class="card">
         <div class="p-1 d-sm-flex">
           <div class="m-3 d-flex flex-column">
@@ -53,79 +51,76 @@
             </div>
           </div>
         </div>
-      </div>
-    </div>
-    
-    @if (auth()->user()->id == $user->id)
-      <div class="col-md-10 mb-3">
-        <div class="col-md-4">
-          <select class="form-control" name="select" onChange="location.href=value;">
-            @foreach ($status_list as $status_id => $status_text)
-            <option @if($request_status_id == $status_id) selected @endif value="{{ url()->current() }}?status={{ $status_id }}">{{ $status_text }}</option>
-            @endforeach
-          </select>
-        </div>
-      </div>
-    @endif
-    
-    <div class="col-md-10 mb-3 row">
-    @if (isset($timelines))
-      @foreach ($timelines as $timeline)
-        <div class="p-3 col-sm-6">
-          <div class="card">
-            <a href="{{ route('articles.show', ['article'=>$timeline->id]) }}">
-              <div class="header-image-wrapper">
-                <div class="header-image-content" style="background-image:url('{{$timeline->header_image}}');"></div>    
-              </div>
-            </a>
-            <div class="card-haeder w-100 p-3">
-              <div class="ml-2 d-flex flex-column">
-                <div class="w-100 d-inline-flex">
-                  <a href="{{ url('users/' .$timeline->user->id) }}" class="text-secondary">
-                    <img src="{{ asset($timeline->user->profile_image) }}" class="rounded" width="39" height="39">
-                  </a>
-                  <a href="{{ route('articles.show', ['article'=>$timeline->id]) }}">
-                    <p class="px-1">{{ $timeline->title }}</p>
-                  </a>
-                </div>
-                <p class="mb-0 text-secondary">
-                  @foreach($timeline->tags as $tag)
-                    <span class="tag-mark">{{$tag->name}}</span>
-                  @endforeach
-                </p>
-                <div class="mt-1 d-flex">
-                  <div class="mr-auto text-secondary">
-                    <span>by &#064;{{$timeline->user->screen_name}}</span>
-                    <span>{{ $timeline->created_at->format('Y-m-d H:i') }}</span>
-                    <span><i class="far fa-thumbs-up"></i>{{ count($timeline->favorites) }}</span>
-                  </div>
-                  @if (auth()->user()->id == $user->id)
-                    <div class="dropdown d-flex align-items-center">
-                      <a href="{{ url('articles/' .$timeline->id. '/edit') }}" role="button" id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                        <i class="fas fa-ellipsis-v fa-fw"></i>
-                      </a>
-                      <div class="dropdown-menu" aria-labelledby="dropdownMenuLink">
-                        <form method="POST" action="{{ url('articles/' .$timeline->id) }}" class="mb-0">
-                          @csrf
-                          @method('DELETE')
 
-                          <a href="{{ url('articles/' .$timeline->id .'/edit') }}" class="dropdown-item">編集</a>
-                          <button type="submit" class="dropdown-item del-btn">削除</button>
-                        </form>
+        @if (auth()->user()->id == $user->id)
+          <div class="col-md-4">
+            <select class="form-control" name="select" onChange="location.href=value;">
+              @foreach ($status_list as $status_id => $status_text)
+              <option @if($request_status_id == $status_id) selected @endif value="{{ url()->current() }}?status={{ $status_id }}">{{ $status_text }}</option>
+              @endforeach
+            </select>
+          </div>
+        @endif
+
+        <div class="row p-3">
+          @if (isset($timelines))
+            @foreach ($timelines as $timeline)
+              <div class="col-sm-6">
+                <div class="card">
+                  <a href="{{ route('articles.show', ['article'=>$timeline->id]) }}">
+                    <div class="header-image-wrapper">
+                      <div class="header-image-content" style="background-image:url('{{$timeline->header_image}}');"></div>    
+                    </div>
+                  </a>
+                  <div class="card-haeder w-100 p-3">
+                    <div class="ml-2 d-flex flex-column">
+                      <div class="w-100 d-inline-flex">
+                        <a href="{{ url('users/' .$timeline->user->id) }}" class="text-secondary">
+                          <img src="{{ asset($timeline->user->profile_image) }}" class="rounded" width="39" height="39">
+                        </a>
+                        <a href="{{ route('articles.show', ['article'=>$timeline->id]) }}">
+                          <p class="px-1">{{ $timeline->title }}</p>
+                        </a>
+                      </div>
+                      <p class="mb-0 text-secondary">
+                        @foreach($timeline->tags as $tag)
+                          <span class="tag-mark">{{$tag->name}}</span>
+                        @endforeach
+                      </p>
+                      <div class="mt-1 d-flex">
+                        <div class="mr-auto text-secondary">
+                          <span>by &#064;{{$timeline->user->screen_name}}</span>
+                          <span>{{ $timeline->created_at->format('Y-m-d H:i') }}</span>
+                          <span><i class="far fa-thumbs-up"></i>{{ count($timeline->favorites) }}</span>
+                        </div>
+                        @if (auth()->user()->id == $user->id)
+                          <div class="dropdown d-flex align-items-center">
+                            <a href="{{ url('articles/' .$timeline->id. '/edit') }}" role="button" id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                              <i class="fas fa-ellipsis-v fa-fw"></i>
+                            </a>
+                            <div class="dropdown-menu" aria-labelledby="dropdownMenuLink">
+                              <form method="POST" action="{{ url('articles/' .$timeline->id) }}" class="mb-0">
+                                @csrf
+                                @method('DELETE')
+                                <a href="{{ url('articles/' .$timeline->id .'/edit') }}" class="dropdown-item">編集</a>
+                                <button type="submit" class="dropdown-item del-btn">削除</button>
+                              </form>
+                            </div>
+                          </div>
+                        @endif
+                        <div class="d-flex align-items-center">
+                          <a href="{{ url('articles/' .$timeline->id) }}#comment"><i class="far fa-comment fa-fw"></i></a>
+                          <p class="mb-0 text-secondary">{{ count($timeline->comments) }}</p>
+                        </div>
                       </div>
                     </div>
-                  @endif
-                  <div class="d-flex align-items-center">
-                    <a href="{{ url('articles/' .$timeline->id) }}#comment"><i class="far fa-comment fa-fw"></i></a>
-                    <p class="mb-0 text-secondary">{{ count($timeline->comments) }}</p>
                   </div>
                 </div>
               </div>
-            </div>
-          </div>
+            @endforeach
+          @endif
         </div>
-      @endforeach
-    @endif
+      </div>
     </div>
   </div>
   <div class="my-4 d-flex justify-content-center">
