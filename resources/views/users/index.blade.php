5dc9c10abbcfb9334daf3e5c976f51919c32b888
chameleon333 <uta3chame@gmail.com>
2020-06-23 19:06:23 +0900

    [update]user.index.balde

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -5,13 +5,13 @@
     <div class="row justify-content-center">
       <div class="col-md-12">
         @if(request()->path() == 'users') 
-          @include('components.user_list')
+          @include('components.users.user_list')
         @else
           <div class="card">
-            @include('components.user_profile')
-            @include('components.user_tab_list')
+            @include('components.users.user_profile')
+            @include('components.users.user_tab_list')
             <div class="p-3">
-              @include('components.user_list')
+              @include('components.users.user_list')
             </div>
           </div>
         @endif
