4ad833eda48226df50ff7b6009b2704414b612a0
chameleon333 <uta3chame@gmail.com>
2020-06-24 13:40:22 +0900

    [add]Test to see if your favorite articles are displayed in users

--- tests/Feature/FavoriteTest.php
+++ tests/Feature/FavoriteTest.php
@@ -1,5 +1,8 @@
 <?php
 
+use App\Models\Article;
+use App\Models\User;
+
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
@@ -39,4 +42,41 @@ class FavoriteTest extends TestCase
             'article_id' => $article_id
         ]);
     }
+
+    public function testDisplayFavoriteArticleInUsers()
+    {
+        $user_id = 1;
+        $favorites = factory(App\Models\Favorite::class,5)->create([
+            'user_id' => $user_id,
+        ]);
+
+        $favoriting_user = User::find($user_id);
+
+        $response = $this->actingAs($favoriting_user);
+        $response = $response->get('/users/'.$user_id.'/favorite');
+
+        foreach($favorites as $favorite) {
+            $favorite_article = Article::find($favorite->article_id);
+            $response->assertSeeText($favorite_article->title);
+        }
+    }
+
+    public function testNotDisplayFavoriteArticleInUsers()
+    {
+        $user_id = 1;
+        $favorites = factory(App\Models\Favorite::class,5)->create([
+            'user_id' => 2,
+        ]);
+
+        $favoriting_user = User::find($user_id);
+
+        $response = $this->actingAs($favoriting_user);
+        $response = $response->get('/users/'.$user_id.'/favorite');
+
+        foreach($favorites as $favorite) {
+            $favorite_article = Article::find($favorite->article_id);
+            $response->assertDontSeeText($favorite_article->title);
+        }
+    }
+
 }
