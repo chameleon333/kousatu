bab5bfa3389b4dff4565d0d49d6520d3f8993be7
chameleon333 <uta3chame@gmail.com>
2020-07-08 16:35:39 +0900

    [update]Pagination test

--- tests/Feature/PaginationTest.php
+++ tests/Feature/PaginationTest.php
@@ -5,6 +5,8 @@ use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use App\Models\User;
+use App\Models\Article;
+use App\Models\Tag;
 
 
 use Tests\TestCase;
@@ -19,11 +21,100 @@ class PaginationTest extends TestCase
      *
      * @return void
      */
-    public function testIndex()
+    public function testPaginationIndex()
     {
-        $response = $this->get('/');
-        $response->assertLocation('/articles');
-        // $response->assertStatus(302);
+        $this->get('/')
+            ->assertStatus(302);
+    }
+    public function testPaginationArticle()
+    {
+        $this->get('/articles')
+            ->assertStatus(200);
+    }
+
+    public function testPaginationArticleShow()
+    {
+        $article = factory(Article::class)->create();
+
+        $this->get("/articles/{$article->id}")
+            ->assertStatus(200);
+    }
+
+    public function testPaginationArticleEdit()
+    {
+        $article = factory(Article::class)->create();
+        $user = User::find($article->user_id);
+
+        $this->actingAs($user)
+            ->get("/articles/{$article->id}/edit")
+            ->assertStatus(200);
+    }
+
+    public function testPaginationArticleCreate()
+    {
+        $article = factory(Article::class)->create();
+        $user = User::find($article->user_id);
+
+        $this->actingAs($user)
+            ->get("/articles/create")
+            ->assertStatus(200);
+    }
+
+    public function testPaginationUsers()
+    {
+
+        $this->get("/users")
+            ->assertStatus(200);
+    }
+
+    public function testPaginationUsersShow()
+    {
+        $user = factory(User::class)->create();
+        $this->get("/users/{$user->id}")
+            ->assertStatus(200);
+    }
+
+    public function testPaginationUsersShowFollowing()
+    {
+        $user = factory(User::class)->create();
+        $this->get("/users/{$user->id}/following")
+            ->assertStatus(200);
+    }
+
+    public function testPaginationUsersShowFollowers()
+    {
+        $user = factory(User::class)->create();
+        $this->get("/users/{$user->id}/followers")
+            ->assertStatus(200);
+    }
+
+    public function testPaginationUsersShowFavorite()
+    {
+        $user = factory(User::class)->create();
+        $this->get("/users/{$user->id}/favorite")
+            ->assertStatus(200);
+    }
+
+    public function testPaginationUsersEdit()
+    {
+        $user = factory(User::class)->create();
+        $this->actingAs($user)
+            ->get("/users/{$user->id}/edit")
+            ->assertStatus(200);
+    }
+
+    public function testPaginationSearch()
+    {
+        $user = factory(User::class)->create();
+        $this->get("/search?keyword=test")
+            ->assertStatus(200);
+    }
+
+    public function testPaginationTag()
+    {
+        $tag = factory(Tag::class)->create();
+        $this->get("/tags/{$tag->id}")
+            ->assertStatus(200);
     }
 
     // //ログイン時、記事投稿画面に遷移する
@@ -31,7 +122,7 @@ class PaginationTest extends TestCase
     {
         $user = factory(User::class)->create();
         $response = $this->actingAs($user)
-                         ->get('/articles/create');
+            ->get('/articles/create');
         $response->assertSee('投稿する');
     }
 
@@ -41,7 +132,4 @@ class PaginationTest extends TestCase
         $response = $this->get('/articles/create');
         $response->assertLocation('/login');
     }
-
-
 }
-
