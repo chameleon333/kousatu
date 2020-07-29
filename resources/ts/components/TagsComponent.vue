dd9a01080670309189a01a389ff7ddcdfaaa642b
chameleon333 <uta3chame@gmail.com>
2020-07-29 21:42:22 +0900

    [update]TagsComponent.vue

--- resources/ts/components/TagsComponent.vue
+++ resources/ts/components/TagsComponent.vue
@@ -1,56 +1,45 @@
 <template>
-    <div>
-        <vue-tags-input
-            v-model="tag"
-            :tags="tags"
-            :max-tags="maxTags"
-            :placeholder="placeholderText"
-            @tags-changed="newTags => tags = newTags"
-        />
-        <div class="tags">
-            <input type="hidden" name="tags[]" v-for="parameter_tag in parameter_tags" v-bind:value="parameter_tag.name">
-        </div>
+  <div>
+    <vue-tags-input
+      v-model="tag"
+      :tags="tags"
+      :max-tags="maxTags"
+      :placeholder="placeholderText"
+      @tags-changed="newTags => tags = newTags"
+    />
+    <div class="tags">
+      <input type="hidden" name="tags[]" v-for="tag in tags" v-bind:value="tag.text" />
     </div>
+  </div>
 </template>
 
 
-<script>
-import VueTagsInput from '@johmun/vue-tags-input';
-export default {
-    components: {
-        VueTagsInput,
-    },
-    data(){
-        //Viewに表示されたタグ名をvueで受け取る
-        var parameter_tags = new Array();
-        var $display_tags = new Array();
-        $('.parameter_tags').each(function() {
-            $display_tags.push({text:$(this).text()});
-            parameter_tags.push({name: $(this).text()});
-        });
-        return {
-            tag: '',
-            tags:$display_tags,
-            parameter_tags: parameter_tags,
-            maxTags:5,
-            placeholderText:'タグは5つまで登録できます'
-        };
-    },
-    watch: {
-        tags: function(tags) {
-            var parameter_tags = new Array();
-            tags.forEach(function(tag){
-                parameter_tags.push({name: tag.text}); 
-            });
-            this.parameter_tags = parameter_tags;
-        },
-    },
-};
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+const VueTagsInput = require("@johmun/vue-tags-input").default;
+@Component({
+  components: {
+    VueTagsInput
+  }
+})
+export default class TagsComponent extends Vue {
+  public tag: string = "";
+  public tags: object;
+  public maxTags: number = 5;
+  public placeholderText: string = "タグは5つまで登録できます";
+
+  created() {
+    this.tags = $(".parameter_tags")
+      .map(function() {
+        return { text: $(this).text() };
+      })
+      .get();
+  }
+}
 </script>
 
 <style scoped>
-    .vue-tags-input{
-        max-width: 100%;
-    }
-
+.vue-tags-input {
+  max-width: 100%;
+}
 </style>
