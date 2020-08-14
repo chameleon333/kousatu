e7970dffa902e2fb8d3e5e80c633b4d4eddd6bc3
chameleon333 <uta3chame@gmail.com>
2020-08-14 21:50:30 +0900

    [add]TypeScript

--- resources/ts/app.ts
+++ resources/ts/app.ts
@@ -1,22 +1,20 @@
 import Vue from 'vue'
 import bootstrap from './bootstrap';
 import AppComponent from './components/App.vue';
-import ArticlListComponent from './components/ArticleListComponent.vue';
-import TagsComponent from './components/TagsComponent.vue';
-import PostArticleButtonComponent from './components/PostArticleButtonComponent.vue';
 import ArticleListComponent from './components/ArticleListComponent.vue';
 import FavoriteButtonComponent from './components/FavoriteButtonComponent.vue';
 import FollowButtonComponent from './components/FollowButtonComponent.vue';
+import PostArticleButtonComponent from './components/PostArticleButtonComponent.vue';
+import TagsComponent from './components/TagsComponent.vue';
 
 bootstrap();
 
 const app: any = new Vue({
     el: '#app',
-    // render: h => h(AppComponent)
     components: {
         'article-list-component': ArticleListComponent,
-        // "tags-component": TagsComponent,
-        // "post-article-button-component": PostArticleButtonComponent,
+        "post-article-button-component": PostArticleButtonComponent,
+        "tags-component": TagsComponent,
         // 'favorite-button-component': FavoriteButtonComponent,
         // 'follow-button-component': FollowButtonComponent,
     },
--- resources/ts/components/PostArticleButtonComponent.vue
+++ resources/ts/components/PostArticleButtonComponent.vue
@@ -1,32 +1,47 @@
 <template>
-    <div class="btn-group">
-        <button id="post-aritcle-button" type="submit" class="btn btn-primary" onclick="callBody();">{{ status_text }}</button>
-        <button type="button" class="btn btn-light dropdown-toggle dropdown-toggle-split" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-            <span class="sr-only">ドロップダウンの切替</span>
-        </button>
-        <input type="hidden" name="article_status_id" v-bind:value="status_num">
-        <div class="dropdown-menu dropdown-menu-right">
-            <span class="dropdown-item" v-for="(status_text,index) in statusTexts" :key="status_text" v-on:click="changePostArticleStatus(status_text,index)">
-                {{ status_text }}
-            </span>
-        </div>
+  <div class="btn-group">
+    <button
+      id="post-aritcle-button"
+      type="submit"
+      class="btn btn-primary"
+      onclick="callBody();"
+    >{{ status_text }}</button>
+    <button
+      type="button"
+      class="btn btn-light dropdown-toggle dropdown-toggle-split"
+      data-toggle="dropdown"
+      aria-haspopup="true"
+      aria-expanded="false"
+    >
+      <span class="sr-only">ドロップダウンの切替</span>
+    </button>
+    <input type="hidden" name="article_status_id" v-bind:value="status_num" />
+    <div class="dropdown-menu dropdown-menu-right">
+      <span
+        class="dropdown-item"
+        v-for="(status_text,index) in statusTexts"
+        :key="status_text"
+        v-on:click="changePostArticleStatus(status_text,index)"
+      >{{ status_text }}</span>
     </div>
+  </div>
 </template>
 
-<script>
-    export default {
-        props: ['statusTexts'],
-        methods: {
-            changePostArticleStatus(status_text,index) {
-                $("#post-aritcle-button").text(status_text);
-                $('input[name="article_status_id"]').val(index);
-            }
-        },
-        data() {
-            return {
-                status_text: this.statusTexts[0],
-                status_num: 0
-            }
-        }
-    }
+<script lang="ts">
+import { Component, Emit, Prop, Vue } from "vue-property-decorator";
+
+@Component
+export default class PostArticleButtonComponent extends Vue {
+  @Prop({ default: "" })
+  statusTexts: string[];
+
+  public status_text: string = this.statusTexts[0];
+  public status_num: number = 0;
+
+  @Emit("changePostArticleStatus")
+  changePostArticleStatus(status_text: string, index: number) {
+    $("#post-article-button").text(status_text);
+    $("input[name='article_status_id'").val(index);
+  }
+}
 </script>
