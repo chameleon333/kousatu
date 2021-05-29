b7941a330f5c8f5cbfe0536fe52da55d3a8df4a7
chameleon333 <uta3chame@gmail.com>
2021-05-29 10:27:20 +0900

    [update] aws_routeはデフォルトでlocalを作成するためlocalの記述を削除&VPCのメインテーブル指定する記述を追加

--- terraform/aws_internet_gateway.tf
+++ terraform/aws_internet_gateway.tf
@@ -12,17 +12,14 @@ resource "aws_route_table" "kousatu" {
   vpc_id = aws_vpc.kousatu.id
 }
 
-resource "aws_route" "local" {
-  gateway_id = aws_internet_gateway.kousatu.id
-  #   destination_cidr_block = "10.0.0.0/16"
-  destination_cidr_block = "0.0.0.0/0"
+resource "aws_route" "kousatu" {
   route_table_id         = aws_route_table.kousatu.id
+  gateway_id             = aws_internet_gateway.kousatu.id
+  destination_cidr_block = "0.0.0.0/0"
   depends_on             = [aws_route_table.kousatu]
 }
 
-resource "aws_route" "kousatu" {
-  gateway_id             = aws_internet_gateway.kousatu.id
-  destination_cidr_block = "0.0.0.0/0"
-  route_table_id         = aws_route_table.kousatu.id
-  depends_on             = [aws_route_table.kousatu]
+resource "aws_main_route_table_association" "a" {
+  vpc_id         = aws_vpc.kousatu.id
+  route_table_id = aws_route_table.kousatu.id
 }
