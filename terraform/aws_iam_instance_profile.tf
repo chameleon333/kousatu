bac443bd1396e4f4f1e91662cef0b121af62e0f1
chameleon333 <uta3chame@gmail.com>
2021-05-30 20:44:05 +0900

    [terraform] ecsとec2のロールを分割&修正

--- terraform/aws_iam_instance_profile.tf
+++ terraform/aws_iam_instance_profile.tf
@@ -1,4 +1,5 @@
 # EC2がECSを利用できるように
 resource "aws_iam_instance_profile" "kousatu" {
   name = "es2_container_service"
+  role = aws_iam_role.ec2-role.name
 }
--- terraform/aws_iam_role.tf
+++ terraform/aws_iam_role.tf
@@ -2,12 +2,27 @@ data "template_file" "task-role-template" {
   template = file("./task_role.json")
 }
 
+data "template_file" "ec2-role-template" {
+  template = file("./ec2_role.json")
+}
+
 resource "aws_iam_role" "task-role" {
-  name               = "ecsTaskExecutionRoleTest"
+  name               = "ecsTaskExecutionRole"
   assume_role_policy = data.template_file.task-role-template.rendered
 }
 
-resource "aws_iam_role_policy_attachment" "task-role-attachment" {
-  role       = aws_iam_role.task-role.name
+resource "aws_iam_role" "ec2-role" {
+  name               = "eC2ContainerServiceEC2Role"
+  assume_role_policy = data.template_file.ec2-role-template.rendered
+}
+
+resource "aws_iam_role_policy_attachment" "ec2-role" {
+  role       = aws_iam_role.ec2-role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonEC2ContainerServiceforEC2Role"
+}
+
+# ecs task
+resource "aws_iam_role_policy_attachment" "task_role" {
   policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+  role       = aws_iam_role.task-role.name
 }
