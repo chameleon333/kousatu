5c920e54ff8cbbc43a746ea000337d6a52a4a614
chameleon333 <uta3chame@gmail.com>
2021-07-25 17:03:24 +0900

    [update] 人気ランキング取得方法変更

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,9 @@
 
 namespace App\Providers;
 
+use Exception;
+use Illuminate\Pagination\LengthAwarePaginator;
+use Illuminate\Support\Collection;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\View;
 
@@ -22,9 +25,61 @@ class AppServiceProvider extends ServiceProvider
      *
      * @return void
      */
-    public function boot()
+    public function boot(Collection $collection)
     {
-        //
         View::share('keyword', '');
+        $this->addCollectionMacro($collection);
+    }
+
+    /**
+     * Collectionのマクロ
+     * @param Collection $collection
+     * @return void
+     */
+    public function addCollectionMacro(Collection $collection)
+    {
+
+        /**
+         *
+         */
+        $collection->macro("rankBy", function (string $key, bool $addRank = true) {
+            return $this->sortByDesc($key)
+                ->values()
+                ->map(function ($item, $index) use ($key, $addRank) {
+                    if (!isset($item[$key])) {
+                        throw new Exception('values used in ranking must not be null.');
+                    }
+
+                    if ($addRank) {
+                        $item["rank"] = $index + 1;
+                    }
+
+                    return $item;
+                });
+        });
+
+
+        /**
+         * Collectionに対して paginate できるようにするマクロ
+         *
+         * @param int $perPage
+         * @param int $total
+         * @param int $page
+         * @param string $pageName
+         * @return array
+         */
+        $collection::macro('paginate', function ($perPage, $total = null, $page = null, $pageName = 'page') {
+            $page = $page ?: LengthAwarePaginator::resolveCurrentPage($pageName);
+            return new LengthAwarePaginator(
+                $this->forPage($page, $perPage)->values(),
+                $total ?: $this->count(),
+                $perPage,
+                $page,
+                [
+                    'path' => LengthAwarePaginator::resolveCurrentPath(),
+                    'pageName' => $pageName,
+                ]
+            );
+        });
     }
 }
