0b37b4e37697743f9596433f454d19defbfa21dd
chameleon333 <uta3chame@gmail.com>
2021-07-25 17:04:25 +0900

    [update] ArticleTest.php

--- tests/Feature/ArticleTest.php
+++ tests/Feature/ArticleTest.php
@@ -3,16 +3,12 @@
 use App\Models\Article;
 use App\Models\Favorite;
 use App\Models\Tag;
-// use Illuminate\Foundation\Testing\RefreshDatabase;
-// use Illuminate\Foundation\Testing\WithFaker;
-// use Illuminate\Foundation\Testing\DatabaseMigrations;
-// use Illuminate\Foundation\Testing\DatabaseTransactions;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
 use Tests\TestCase;
 
 class ArticleTest extends TestCase
 {
-    // use DatabaseMigrations;
-    // use DatabaseTransactions;
+    use DatabaseTransactions;
     /**
      * A basic feature test example.
      *
